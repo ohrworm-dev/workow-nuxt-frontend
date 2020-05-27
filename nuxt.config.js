570bf9ff8d0b1790605dd15317173a7482e914c1
Greenfig <rene_anderson@hotmail.com>
2020-05-27 11:55:39 -0400

    tailwind ui added

--- nuxt.config.js
+++ nuxt.config.js
@@ -31,7 +31,7 @@ module.exports = {
     /*
      ** Plugins to load before mounting the App
      */
-    plugins: ['~/plugins/tailwind.js'],
+    plugins: [],
     /*
      ** Nuxt.js dev-modules
      */
--- plugins/tailwind.js
+++
@@ -1,6 +0,0 @@
-import Vue from 'vue'
-import Tailwind from 'vue-tailwind'
-
-Vue.use(Tailwind)
-
-// https://vue-tailwind.com/
