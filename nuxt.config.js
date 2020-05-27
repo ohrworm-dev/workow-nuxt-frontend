c0f7cd23bf451af4a3f2a29daf309cfbca2372d4
Greenfig <rene_anderson@hotmail.com>
2020-05-27 12:53:26 -0400

    auth route fix

--- nuxt.config.js
+++ nuxt.config.js
@@ -63,9 +63,9 @@ module.exports = {
         strategies: {
             local: {
                 endpoints: {
-                    login: { url: '/api/auth/login', method: 'post', propertyName: 'token' },
-                    logout: { url: '/api/auth/logout', method: 'delete' },
-                    user: { url: '/api/auth/user', method: 'get', propertyName: 'user' }
+                    login: { url: 'api/auth/login', method: 'post', propertyName: 'token' },
+                    logout: { url: 'api/auth/logout', method: 'delete' },
+                    user: { url: 'api/auth/user', method: 'get', propertyName: 'user' }
                 }
             }
         }
